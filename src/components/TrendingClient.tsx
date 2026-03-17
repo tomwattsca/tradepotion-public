635afb00a749fcf4f10aeabc7baef474ab97bc0c
Tom Watts <tom@topshelfmedia.ca>
2026-03-17 05:20:00 +0000

    fix: remove unused formatMarketCap import in TrendingClient (ESLint build fail)

--- src/components/TrendingClient.tsx
+++ src/components/TrendingClient.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { formatPrice, formatMarketCap, formatPct, pctColor } from '@/lib/utils';
+import { formatPrice, formatPct, pctColor } from '@/lib/utils';
 
 interface TrendingCoin {
   id: string;
